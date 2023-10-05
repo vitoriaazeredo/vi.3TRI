3c338390668fee48ae9efe3999e6141a7495feba
vitória <114578603+vitoriaazeredo@users.noreply.github.com>
2023-10-05 12:18:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ function tocaSomPom() {
     document.querySelector('som_tecla_pom').play();
   }
   document.querySelector('.tecla_pom');
+  
+  document.querySelector('.tecla_pom').onclick = tocaSomPom;
+  
   document.querySelectorALL('.tecla')
+  
