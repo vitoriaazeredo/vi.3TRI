439f44d2c352e012b2d0e7a84f6264995de2e242
vitória <114578603+vitoriaazeredo@users.noreply.github.com>
2023-11-16 08:39:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
 
+const listaDeTeclas = document.querySelectorAll ('.tecla');
+
+//para
+
+for (let contador = 0; contador < listaDeTeclas.lenght; contador++);
+
+const tecla = listaDeTeclas [contador];
+const instrumento = tecla.classList[1];
+const idAudio = '#som_s(instrumento)'; //template string
+
+tecla.onclick = function () {
+    tocaSom(idAudio);
+}
+
+tecla.onkeydown = function () {
+    tecla.classlist.add('ativa');
+}
+
+//enquanto
+while (contador < listaDeTeclas.length ){
+
+listaDeTeclas{0}.onclick = tocaSomPom;
+
+contador= contador + 1;
+
+console.log(contador);
+
+}
+
+
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function (){
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;  
+}
