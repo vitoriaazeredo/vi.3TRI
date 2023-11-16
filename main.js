d72a27165e5e796ab3f4715da92694b94fa24912
vitória <114578603+vitoriaazeredo@users.noreply.github.com>
2023-11-16 08:38:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-function tocaSomPom() {
-    document.querySelector('som_tecla_pom').play();
-  }
-  document.querySelector('.tecla_pom');
-  
-  document.querySelector('.tecla_pom').onclick = tocaSomPom;
-  
-  document.querySelectorALL('.tecla')
-  
-  const listaDeTeclas = document.querySelectorALL(.'tecla');
+
